0172018392245d31da1f5887531cb2164c479a44
Sweeney Ngo <sweeneyngo@gmail.com>
2022-05-16 23:47:52 -0700

    Added demo button w/ custom modal

---
+++ src/components/AlertInbox/Popup.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export interface PopupProps {
+    title: string;
+    show: boolean;
+    onClose: () => void;
+    children?: JSX.Element | string;
+}
+
+function Popup(props: PopupProps): JSX.Element {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+        props.onClose();   
+    }
+
+    useEffect(() => {
+        setShow(props.show);
+    }, [props.show]);
+
+    return (
+        <div style={{ visibility: show ? "visible" : "hidden", opacity: show ? "1" : "0"}} className="overlay">
+            
+            <div className="popup">
+                <h2>{props.title}</h2>
+                <div className="content">{props.children}</div>
+                <div className="close" onClick={handleClose}>let's go!</div>
+            </div>
+        </div>
+    );
+}
+
+export default Popup;
--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,14 +1,27 @@
 import { FC } from 'react';
+import AppWrapper from '../components/AppWrapper';
+import AlertInbox from "../components/AlertInbox";
+import { HeaderSections } from '../types/globalTypes';
+import '../assets/WestwoodSans-Regular.ttf';
+import '../styles/Demo.scss';
 
 const Demo: FC = () => {
   return (
-    <>
-      <h1>Demo</h1>
-      <p>
-        The main animation slide, where we have the shelves of boxes and the
-        robot.
-      </p>
-    </>
+    <div>
+      <AppWrapper section={HeaderSections.LEARNING_SECTION}>
+        <div id="layout">
+          <h1 className="header">Demo</h1>
+          <p className="description">
+            The main animation slide, where we have the shelves of boxes and the
+            robot.
+          </p>
+
+          <div id="inbox-wrapper">
+            <AlertInbox section_no={1}/>
+          </div>
+        </div>
+    </AppWrapper>
+    </div>
   );
 };
 
