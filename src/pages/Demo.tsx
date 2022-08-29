413158c299949517ff26e54f6a1d79510138b39b
Daniel Kao <78634083+Dankco@users.noreply.github.com>
2022-08-28 22:13:17 -0400

    Shelf Addresses Added

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,17 +1,22 @@
 import { FC } from 'react';
 import AlertInbox from '../components/AlertInbox';
 import AppWrapper from '../components/AppWrapper';
+import ShelfAddress from '../components/ShelfAddress';
 import { HeaderSections } from '../types/globalTypes';
 import '../assets/WestwoodSans-Regular.ttf';
 import '../styles/Demo.scss';
 
 const Demo: FC = () => {
+  const nums = Array.from({length: 24}, (_, index) => index + 1);
   return (
     <div>
       <AppWrapper section={HeaderSections.LEARNING_SECTION}>
         <div id="layout">
           <h1 className="header">Demo</h1>
           <p className="description">
+            {nums.map(num =>
+              <ShelfAddress num = {num}></ShelfAddress>
+            )}
             The main animation slide, where we have the shelves of boxes and the
             robot.
           </p>
