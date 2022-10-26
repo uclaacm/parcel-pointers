4f0b314aa7499e8296d0e1b8ee72ab0857e3454f
Daniel Kao <78634083+Dankco@users.noreply.github.com>
2022-10-25 17:20:51 -0700

    add box component with 6 different types/widths

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import AlertInbox from '../components/AlertInbox';
 import AppWrapper from '../components/AppWrapper';
+import Box from '../components/Box';
 import { HeaderSections } from '../types/globalTypes';
 import '../assets/WestwoodSans-Regular.ttf';
 import '../styles/Demo.scss';
@@ -11,11 +12,12 @@ const Demo: FC = () => {
       <AppWrapper section={HeaderSections.LEARNING_SECTION}>
         <div id="layout">
           <h1 className="header">Demo</h1>
-          <p className="description">
-            The main animation slide, where we have the shelves of boxes and the
-            robot.
-          </p>
-
+          <Box letter="a" num={1}></Box>
+          <Box letter="b" num={2}></Box>
+          <Box letter="c" num={3}></Box>
+          <Box letter="d" num={4}></Box>
+          <Box letter="e" num={5}></Box>
+          <Box letter="f" num={6}></Box>
           <div id="inbox-wrapper">
             <AlertInbox section_no={1} />
           </div>
