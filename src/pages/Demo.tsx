082818d860e317d5af08690d165e10ec133b178e
Daniel Kao <78634083+Dankco@users.noreply.github.com>
2022-11-01 18:30:50 -0700

    define width and height of shelf addresses

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 import AlertInbox from '../components/AlertInbox';
 import AppWrapper from '../components/AppWrapper';
-import ShelfAddress from '../components/ShelfAddress';
 import Box from '../components/Box';
+import ShelfAddress from '../components/ShelfAddress';
 import { HeaderSections } from '../types/globalTypes';
 import '../assets/WestwoodSans-Regular.ttf';
 import '../styles/Demo.scss';
@@ -14,10 +14,15 @@ const Demo: FC = () => {
   const nums3 = nums.slice(48, 72);
   return (
     <div>
-      <AppWrapper section={HeaderSections.LEARNING_SECTION}>
+      <AppWrapper section={HeaderSections.DEMO_SECTION}>
         <div id="layout">
-          <h1 className="header">Demo</h1>
           <p className="description">
+            <Box letter="a" num={1}></Box>
+            <Box letter="b" num={2}></Box>
+            <Box letter="c" num={3}></Box>
+            <Box letter="d" num={4}></Box>
+            <Box letter="e" num={5}></Box>
+            <Box letter="f" num={6}></Box>
             {nums1.map((num) => (
               <ShelfAddress key={null} num={num}></ShelfAddress>
             ))}
@@ -30,16 +35,7 @@ const Demo: FC = () => {
               <ShelfAddress key={null} num={num}></ShelfAddress>
             ))}
             <br></br>
-            The main animation slide, where we have the shelves of boxes and the
-            robot.
           </p>
-
-          <Box letter="a" num={1}></Box>
-          <Box letter="b" num={2}></Box>
-          <Box letter="c" num={3}></Box>
-          <Box letter="d" num={4}></Box>
-          <Box letter="e" num={5}></Box>
-          <Box letter="f" num={6}></Box>
           <div id="inbox-wrapper">
             <AlertInbox section_no={1} />
           </div>
