26dd76913b56d3bf5cf760325993eddc6e93cb12
Ollie Pai <ollie.pai2003@gmail.com>
2023-01-24 18:48:38 -0800

    feat: create empty lesson 3 page and route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Demo from './pages/Demo';
 import Error404 from './pages/Error404';
 import Home from './pages/Home';
 import Lesson1 from './pages/Lesson1';
+import Lesson3 from './pages/Lesson3';
 
 function App(): JSX.Element {
   return (
@@ -12,6 +13,7 @@ function App(): JSX.Element {
           <Route path="/" element={<Home />} />
           <Route path="/demo" element={<Demo />} />
           <Route path="/lesson-1" element={<Lesson1 />} />
+          <Route path="/lesson-3" element={<Lesson3 />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Lesson3.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Lesson3: FC = () => {
+  return (
+    <>
+      <h1>Lesson 3</h1>
+    </>
+  );
+};
+
+export default Lesson3;
