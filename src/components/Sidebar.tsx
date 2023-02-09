b242ef8a26a0fc74235fbb5c44df45177aeff880
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-02-08 23:59:42 -0800

    header highlighting across entire menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,12 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { SetStateAction, useState, Dispatch } from 'react';
 
+// TODO: fix vertical alignment of headers
+// TODO: fix active dropdown
+// TODO: fix dropdown children spacing
+// TODO: fix text animation, weird rn
+// TODO: remove <a> styling for links
+
 interface sidebarProps {
   showMenu: boolean;
   setShowMenu: Dispatch<SetStateAction<boolean>>;
@@ -11,18 +17,34 @@ interface sidebarProps {
 
 interface dropdownProps {
   title: string;
-  list: { name: string, link: string }[];
+  list: { name: string; link: string }[];
 }
 
 function Dropdown(props: dropdownProps): JSX.Element {
   const [expand, setExpand] = useState(false);
   return (
-    <div>
-      <div className="dropdown-header" onClick={() => setExpand(!expand)} >
-        <span style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: expand ? '#FEC9004D' : 'transparent'}} >
+    <div style={{ backgroundOrigin: 'content-box' }}>
+      <div
+        className="dropdown-header"
+        onClick={() => setExpand(!expand)}
+        style={{
+          cursor: 'pointer',
+          backgroundColor: expand ? '#FEC9004D' : 'transparent',
+        }}
+      >
+        <span
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
           <div>{props.title}</div>
-          <div >
-            {!expand ? <ExpandMoreIcon fontSize='large' /> : <ExpandLessIcon fontSize='large' />}
+          <div>
+            {!expand ? (
+              <ExpandMoreIcon fontSize="large" />
+            ) : (
+              <ExpandLessIcon fontSize="large" />
+            )}
           </div>
         </span>
       </div>
@@ -30,15 +52,14 @@ function Dropdown(props: dropdownProps): JSX.Element {
         className="dropdown-children"
         style={{ display: expand ? 'block' : 'none' }}
       >
-        {
-          props.list.map(function (elem) {
-            return (
-              <div>
-                <a href={elem.link}>{elem.name}</a><br />
-              </div>
-            );
-          })
-        }
+        {props.list.map(function (elem, index) {
+          return (
+            <div key={index}>
+              <a href={elem.link}>{elem.name}</a>
+              <br />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
@@ -48,7 +69,7 @@ export default function Sidebar(props: sidebarProps): JSX.Element {
   const lessonList = [
     {
       name: 'Lesson 1',
-      link: '#lesson-1'
+      link: '#lesson-1',
     },
     {
       name: 'Lesson 2',
@@ -57,13 +78,13 @@ export default function Sidebar(props: sidebarProps): JSX.Element {
     {
       name: 'Lesson 3',
       link: '#lesson-3',
-    }
+    },
   ];
 
   const exampleList = [
     {
       name: 'Example 1',
-      link: '#example-1'
+      link: '#example-1',
     },
     {
       name: 'Example 2',
@@ -72,21 +93,28 @@ export default function Sidebar(props: sidebarProps): JSX.Element {
     {
       name: 'Example 3',
       link: '#example-3',
-    }
+    },
   ];
 
   return (
-    <div id="Sidebar" style={{ width: 357/*props.showMenu ? 357 : 0 */ }}>
-      <div className='sidebar-container'>
-        <span style={{ display: 'flex', justifyContent: 'space-between', paddingBottom: 35 }}>
-          <a className='title'>Parcel Pointers</a>
-          <button className="close-btn" onClick={() => props.setShowMenu(false)}>
-            <CloseIcon fontSize='medium' />
-          </button>
-        </span>
-        <Dropdown title="Lessons" list={lessonList} />
-        <Dropdown title="Examples" list={exampleList} />
-      </div>
+    <div className="sidebar" style={{ width: props.showMenu ? 357 : 0 }}>
+      {/* <div className="sidebar-container"> */}
+      <span
+        className="sidebar-header"
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          paddingBottom: 35,
+        }}
+      >
+        <h2 style={{ cursor: 'pointer' }}>Parcel Pointers</h2>
+        <button className="close-btn" onClick={() => props.setShowMenu(false)}>
+          <CloseIcon fontSize="large" />
+        </button>
+      </span>
+      <Dropdown title="Lessons" list={lessonList} />
+      <Dropdown title="Examples" list={exampleList} />
     </div>
+    // </div>
   );
 }
