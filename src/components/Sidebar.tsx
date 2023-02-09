2e701e63f527f7e58f28cc7ad9cdd9016fbe7b5f
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-02-08 23:15:09 -0800

    fix lint errors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,8 @@
 import '../styles/Sidebar.scss';
-import { SetStateAction, useState, Dispatch } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { SetStateAction, useState, Dispatch } from 'react';
 
 interface sidebarProps {
   showMenu: boolean;
@@ -11,7 +11,7 @@ interface sidebarProps {
 
 interface dropdownProps {
   title: string;
-  children: { name: string, link: string }[];
+  list: { name: string, link: string }[];
 }
 
 function Dropdown(props: dropdownProps): JSX.Element {
@@ -19,7 +19,7 @@ function Dropdown(props: dropdownProps): JSX.Element {
   return (
     <div>
       <div className="dropdown-header" onClick={() => setExpand(!expand)} >
-        <span style={{ display: 'flex', justifyContent: "space-between", backgroundColor: expand ? "#FEC9004D" : 'transparent'}} >
+        <span style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: expand ? '#FEC9004D' : 'transparent'}} >
           <div>{props.title}</div>
           <div >
             {!expand ? <ExpandMoreIcon fontSize='large' /> : <ExpandLessIcon fontSize='large' />}
@@ -31,12 +31,12 @@ function Dropdown(props: dropdownProps): JSX.Element {
         style={{ display: expand ? 'block' : 'none' }}
       >
         {
-          props.children.map(function (elem) {
+          props.list.map(function (elem) {
             return (
               <div>
                 <a href={elem.link}>{elem.name}</a><br />
               </div>
-            )
+            );
           })
         }
       </div>
@@ -45,47 +45,47 @@ function Dropdown(props: dropdownProps): JSX.Element {
 }
 
 export default function Sidebar(props: sidebarProps): JSX.Element {
-  const lessonChildren = [
+  const lessonList = [
     {
-      name: "Lesson 1",
-      link: "#lesson-1"
+      name: 'Lesson 1',
+      link: '#lesson-1'
     },
     {
-      name: "Lesson 2",
-      link: "#lesson-2",
+      name: 'Lesson 2',
+      link: '#lesson-2',
     },
     {
-      name: "Lesson 3",
-      link: "#lesson-3",
+      name: 'Lesson 3',
+      link: '#lesson-3',
     }
   ];
 
-  const exampleChildren = [
+  const exampleList = [
     {
-      name: "Example 1",
-      link: "#example-1"
+      name: 'Example 1',
+      link: '#example-1'
     },
     {
-      name: "Example 2",
-      link: "#example-2",
+      name: 'Example 2',
+      link: '#example-2',
     },
     {
-      name: "Example 3",
-      link: "#example-3",
+      name: 'Example 3',
+      link: '#example-3',
     }
   ];
 
   return (
     <div id="Sidebar" style={{ width: 357/*props.showMenu ? 357 : 0 */ }}>
       <div className='sidebar-container'>
-        <span style={{ display: 'flex', justifyContent: "space-between", paddingBottom: 35 }}>
+        <span style={{ display: 'flex', justifyContent: 'space-between', paddingBottom: 35 }}>
           <a className='title'>Parcel Pointers</a>
           <button className="close-btn" onClick={() => props.setShowMenu(false)}>
             <CloseIcon fontSize='medium' />
           </button>
         </span>
-        <Dropdown title="Lessons" children={lessonChildren} />
-        <Dropdown title="Examples" children={exampleChildren} />
+        <Dropdown title="Lessons" list={lessonList} />
+        <Dropdown title="Examples" list={exampleList} />
       </div>
     </div>
   );
