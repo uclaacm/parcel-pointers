f14817447662407e1e36362fca4a05ffff12bee0
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-02-24 21:51:41 -0800

    fix PointerMotivation styling, remove styling from PointerMotivation.scss

--- src/pages/PointerMotivation.tsx
+++ src/pages/PointerMotivation.tsx
@@ -2,21 +2,22 @@ import { FC } from 'react';
 import AppWrapper from '../components/AppWrapper';
 import NavButtons from '../components/NavButtons';
 import { HeaderSections } from '../types/globalTypes';
-import '../styles/PointerMotivation.scss';
 
 const PointerMotivation: FC = () => {
   return (
     <div>
       <AppWrapper section={HeaderSections.TITLE_SECTION}>
-        <div id="lesson">
-          <h1 id="title">Why Pointers?</h1>
-          <p id="description">
+        <div className='page-wrapper'>
+          <h1>Why Pointers?</h1>
+          <p>
             But first, why do we even need pointers? Even though they&apos;re
             annoying to learn, pointers are actually really useful. Click
             through to see the many different ways they can be helpful:
           </p>
-          {/* insert slide show and prev/next buttons */}
-          <p id="description">
+          <div className='diagram'>
+            {/* insert slide show and prev/next buttons */}
+          </div>
+          <p>
             Now that we know pointers are actually gonna be useful in the
             future, let&apos;s see what they are and how they work! Our little
             friend PiPi is going to be your guide through this learning lab
