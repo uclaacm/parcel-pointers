0fc5e84ec2641ccf684b1bb48b52922ce494ee2a
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-02-24 21:23:42 -0800

    add terminal and arrow diagram

--- src/pages/Lesson3.tsx
+++ src/pages/Lesson3.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react';
 import AppWrapper from '../components/AppWrapper';
+import FillArrow from '../components/FillArrow';
 import NavButtons from '../components/NavButtons';
+import Terminal from '../components/Terminal';
 import { HeaderSections } from '../types/globalTypes';
+
+const code1 = `
+  & // = "get the address of"
+  `;
+
+const code2 = `
+  &x // = "get the address of" x
+  &myValue // = "get the address of" myValue
+  &start // = "get the address of" start
+  `;
+
+const code3 = `
+  int myVariable;
+  int* myPointer = &myVariable;
+  `;
 
 const Lesson3: FC = () => {
   return (
@@ -18,35 +35,45 @@ const Lesson3: FC = () => {
             of another variable. There is a special operation that we can use to
             ask C++ to fetch the address of any variable:
           </p>
-          <div className="diagram">terminal 1</div>
+          <div className="diagram">
+            <Terminal code={code1} />
+          </div>
           <p>
             To use this operator, we just put it in front of any variable that
             we have created:
           </p>
-          <div className="diagram">terminal 2</div>
+          <div className="diagram">
+            <Terminal code={code2} />
+          </div>
           <p>
-            If you put & in front of any variable, it will return the memory
-            address of that variable. And now that it has returned a value, you
-            can treat it just like any other value by assigning it to a
-            variable. You can assign this address value to a pointer variable
-            like this:
+            If you put <span className="code">&</span> in front of any variable,
+            it will return the memory address of that variable. And now that it
+            has returned a value, you can treat it just like any other value by
+            assigning it to a variable. You can assign this address value to a
+            pointer variable like this:
           </p>
-          <div className="diagram">terminal 3</div>
+          <div className="diagram">
+            <Terminal code={code3} />
+          </div>
           <p>
-            In the above code, myPointer will now store the address of
-            myVariable:
+            In the above code, <span className="code">myPointer</span> will now
+            store the address of <span className="code">myVariable</span>:
           </p>
-          <div className="diagram">pointer animation</div>
+          <div className="diagram">
+            <FillArrow text1="myPointer" text2="myVariable" />
+          </div>
           <p>
-            The & operator gives you the address of any variable you&apos;ve
-            created. In our warehouse, it&apos;s like Pipi asking you for the
-            address of the box he wants to go to. Or it&apos;s like searching on
-            Google for the address of a restaurant you want to go to.
+            The <span className="code">&</span> operator gives you the address
+            of any variable you&apos;ve created. In our warehouse, it&apos;s
+            like Pipi asking you for the address of the box he wants to go to.
+            Or it&apos;s like searching on Google for the address of a
+            restaurant you want to go to.
           </p>
           <h2>Note</h2>
           <p>
-            If you try to print out the value of a memory address using cout,
-            you might see that the value looks something like 0x7ffde45c9a54
+            If you try to print out the value of a memory address using{' '}
+            <span className="code">cout</span>, you might see that the value
+            looks something like: <span className="code">0x7ffde45c9a54</span>
           </p>
           <p>
             Don&apos;t be alarmed by this - this is an actual memory address in
@@ -54,11 +81,12 @@ const Lesson3: FC = () => {
             addresses get to be very large numbers, and these numbers can be
             really long. To represent large numbers in a more compact way,
             computers use a numbering system called hexadecimal. You can tell
-            that this is a hexadecimal number because the address begins with 0x
-            at the front, and among the numbers you will also find the letters
-            a-f. You&apos;ll learn more about this in a later class!
+            that this is a hexadecimal number because the address begins with{' '}
+            <span className="code">0x</span> at the front, and among the numbers
+            you will also find the letters A-F. You&apos;ll learn more about
+            this in a later class!
           </p>
-          <NavButtons page={5}></NavButtons>
+          <NavButtons page={6}></NavButtons>
         </div>
       </AppWrapper>
     </>
