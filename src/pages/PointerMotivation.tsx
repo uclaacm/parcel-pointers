7a4ffaf5257f8510359000162b111c8b7554a3f2
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-02-24 21:52:03 -0800

    lint fix

--- src/pages/PointerMotivation.tsx
+++ src/pages/PointerMotivation.tsx
@@ -7,14 +7,14 @@ const PointerMotivation: FC = () => {
   return (
     <div>
       <AppWrapper section={HeaderSections.TITLE_SECTION}>
-        <div className='page-wrapper'>
+        <div className="page-wrapper">
           <h1>Why Pointers?</h1>
           <p>
             But first, why do we even need pointers? Even though they&apos;re
             annoying to learn, pointers are actually really useful. Click
             through to see the many different ways they can be helpful:
           </p>
-          <div className='diagram'>
+          <div className="diagram">
             {/* insert slide show and prev/next buttons */}
           </div>
           <p>
