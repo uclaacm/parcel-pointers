1a27b2cf38a516ecabb5706eca3f32baca9b785b
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-03-03 00:13:18 -0800

    check button and expand funcionality

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,7 @@
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { useState } from 'react';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import '../styles/Question.scss';
 
 interface QuestionProps {
@@ -9,14 +12,55 @@ interface QuestionProps {
 }
 
 function Question(props: QuestionProps): JSX.Element {
+  const [correct, setCorrect] = useState(false);
+  const [expand, setExpand] = useState(false);
+
   return (
     <div className="question">
-      <span className="question-text-wrapper">
-        <CheckCircleIcon fontSize="large" className="icon" />
-        {props.children}
-        {/* TODO: conditionally display check/arrow here */}
-      </span>
-      {/* TODO: conditionally display icon + props.correct or props.incorrect */}
+      <div className='question-container-wrapper'>
+        <div className="question-wrapper">
+          <CheckCircleIcon
+            fontSize="large"
+            className="icon"
+            style={correct ? { color: "#31A74B" } : { color: "#817F7F" }}
+          />
+          <div className='question-text-wrapper'>
+            {props.children}
+          </div>
+        </div>
+        <div className='question-wrapper'>
+          {correct ? (
+            expand ? (
+              <div onClick={() => setExpand(!expand)}>
+                <ExpandLessIcon fontSize='large' />
+              </div>
+            ) : (
+              <div onClick={() => setExpand(!expand)}>
+                <ExpandMoreIcon fontSize='large' />
+              </div>
+            )
+          ) : (
+            <button
+              className='check-button'
+              onClick={() => {
+                // TODO: check input and set correct accordingly
+                setCorrect(!correct); // comment this line to check incorrect text
+                setExpand(true);
+              }}
+            >
+              <p className="check-button-text">Check</p>
+            </button>
+          )}
+        </div>
+      </div>
+      {expand ? (
+        correct ? (
+          <p className='correct-text'>{props.correct}</p>
+        ) : (
+          <p className='incorrect-text'>{props.incorrect}</p>
+        )
+      ) : ("")}
+      
     </div>
   );
 }
