45c77f5369df963445634bb6e000f53b72ee08d0
Daniel Kao <dack8@outlook.com>
2023-03-03 20:56:16 -0800

    add dropdowns to Excercise 1 Page

--- src/pages/Exercise1.tsx
+++ src/pages/Exercise1.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import AppWrapper from '../components/AppWrapper';
+import Dropdown from '../components/Dropdown';
 import NavButtons from '../components/NavButtons';
 import Question from '../components/Question';
 import { HeaderSections } from '../types/globalTypes';
@@ -29,7 +30,16 @@ const Exercise1: FC = () => {
             >
               <p className="question-text">
                 The address of the Box B is{' '}
-                <span>{/* TODO: insert dropdown*/}</span>
+                <span className="dropdown-wrapper">
+                  <Dropdown
+                    options={[
+                      { id: 1, name: '10', displayName: '10' },
+                      { id: 2, name: '11', displayName: '11' },
+                      { id: 3, name: '12', displayName: '12' },
+                      { id: 4, name: '13', displayName: '13' },
+                    ]}
+                  />
+                </span>
               </p>
             </Question>
             <Question
@@ -38,7 +48,16 @@ const Exercise1: FC = () => {
               incorrect="Not quite. Think about ... Please try again."
             >
               <p className="question-text">
-                Pipi needs to move <span>{/* TODO: insert dropdown*/}</span>{' '}
+                Pipi needs to move{' '}
+                <span className="dropdown-wrapper">
+                  <Dropdown
+                    options={[
+                      { id: 1, name: '7', displayName: '7' },
+                      { id: 2, name: '8', displayName: '8' },
+                      { id: 3, name: '9', displayName: '9' },
+                    ]}
+                  />
+                </span>{' '}
                 addresses from her current position to get to Box B.
               </p>
             </Question>
@@ -55,7 +74,16 @@ const Exercise1: FC = () => {
             >
               <p className="question-text">
                 The address of the Box E is{' '}
-                <span>{/* TODO: insert dropdown*/}</span>
+                <span className="dropdown-wrapper">
+                  <Dropdown
+                    options={[
+                      { id: 1, name: '20', displayName: '20' },
+                      { id: 2, name: '21', displayName: '21' },
+                      { id: 3, name: '22', displayName: '22' },
+                      { id: 4, name: '23', displayName: '23' },
+                    ]}
+                  />
+                </span>
               </p>
             </Question>
             <Question
@@ -64,7 +92,16 @@ const Exercise1: FC = () => {
               incorrect="Not quite. Think about ... Please try again."
             >
               <p className="question-text">
-                Pipi needs to move <span>{/* TODO: insert dropdown*/}</span>{' '}
+                Pipi needs to move{' '}
+                <span className="dropdown-wrapper">
+                  <Dropdown
+                    options={[
+                      { id: 1, name: '7', displayName: '7' },
+                      { id: 2, name: '8', displayName: '8' },
+                      { id: 3, name: '9', displayName: '9' },
+                    ]}
+                  />
+                </span>{' '}
                 addresses from her current position to get to Box E.
               </p>
             </Question>
