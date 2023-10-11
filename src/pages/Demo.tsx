a37b5b06be8a11968c07a02a69c45423487fee53
Dankco <78634083+Dankco@users.noreply.github.com>
2023-10-11 10:35:35 -0700

    103 Add Grid Alignment Component (#258)
    * add grid component with boxes
    
    * fix lint
    
    * add space
    
    * add components to grid

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -6,9 +6,9 @@ import RightLadder from '../../public/RightLadder.png';
 import AlertInbox from '../components/AlertInbox';
 import AppWrapper from '../components/AppWrapper';
 import Box from '../components/Box';
+import Grid from '../components/Grid';
 import HintBox from '../components/HintBox';
 import NavButtons from '../components/NavButtons';
-import ShelfAddress from '../components/ShelfAddress';
 import { HeaderSections } from '../types/globalTypes';
 import '../assets/WestwoodSans-Regular.ttf';
 import '../styles/Demo.scss';
@@ -316,50 +316,64 @@ const Demo: FC = () => {
   const nums1 = nums.slice(0, 24);
   const nums2 = nums.slice(24, 48);
   const nums3 = nums.slice(48, 72);
+  const itemSpaceArray1 = [6, 3, 2, 2, 2, 3, 4, 1, 3];
+  const itemSpaceArray2 = [5, 1, 6, 2, 3, 4, 3];
+  const itemSpaceArray3 = [2, 5, 2, 4, 1, 3, 1, 4];
   return (
     <div>
       <AppWrapper section={HeaderSections.DEMO_SECTION}>
         <div className="page-wrapper">
           <HintBox text="Try counting the number of boxes PiPi has to visit." />
           <HintBox text="This is my really long hint box text! You made a lot of mistakes lol.This is my really long hint box text! You made a lot of mistakes lol.This is my really long hint box text! You made a lot of mistakes lol.This is my really long hint box text! You made a lot of mistakes lol.This is my really long hint box text! You made a lot of mistakes lol.This is my really long hint box text! You made a lot of mistakes lol." />
+          <Grid
+            addressNums={nums1}
+            itemSpaceArray={itemSpaceArray1}
+            size={40}
+            handleCorrect={setConfetti}
+          >
+            <img className="pipi" src={Pipi} alt="Pipi"></img>
+            <Box letter="a" num={3} conf={false}></Box>
+            <div></div>
+            <Box letter="b" num={2} conf={false}></Box>
+            <div></div>
+            <Box letter="c" num={3} conf={false}></Box>
+            <Box letter="d" num={4} conf={false}></Box>
+            <div></div>
+            <Box letter="e" num={2} conf={false}></Box>
+          </Grid>
+          <Grid
+            addressNums={nums2}
+            itemSpaceArray={itemSpaceArray2}
+            size={40}
+            handleCorrect={setConfetti}
+          >
+            <Box letter="f" num={5} conf={false}></Box>
+            <div></div>
+            <Box letter="g" num={6} conf={false}></Box>
+            <Box letter="h" num={2} conf={false}></Box>
+            <div></div>
+            <Box letter="i" num={4} conf={confetti}></Box>
+            <Box letter="j" num={1} conf={confetti}></Box>
+          </Grid>
+          <Grid
+            addressNums={nums3}
+            itemSpaceArray={itemSpaceArray3}
+            size={40}
+            handleCorrect={setConfetti}
+          >
+            <div></div>
+            <Box letter="k" num={5} conf={false}></Box>
+            <div></div>
+            <Box letter="l" num={4} conf={false}></Box>
+            <Box letter="m" num={1} conf={false}></Box>
+            <Box letter="n" num={3} conf={false}></Box>
+            <div></div>
+            <Box letter="o" num={4} conf={false}></Box>
+          </Grid>
           <p className="description">
-            {nums1.map((num) => (
-              <ShelfAddress
-                key={null}
-                num={num}
-                handleCorrect={setConfetti}
-              ></ShelfAddress>
-            ))}
-            <br></br>
-            {nums2.map((num) => (
-              <ShelfAddress
-                key={null}
-                num={num}
-                handleCorrect={setConfetti}
-              ></ShelfAddress>
-            ))}
-            <br></br>
-            {nums3.map((num) => (
-              <ShelfAddress
-                key={null}
-                num={num}
-                handleCorrect={setConfetti}
-              ></ShelfAddress>
-            ))}
             <br></br>
           </p>
           <h1 className="header">Demo</h1>
-          <Box letter="a" num={1} conf={confetti}></Box>
-          {confetti ? (
-            <Box letter="b" num={2} conf={true}></Box>
-          ) : (
-            <Box letter="b" num={2} conf={false}></Box>
-          )}
-          <Box letter="c" num={3} conf={false}></Box>
-          <Box letter="d" num={4} conf={false}></Box>
-          <Box letter="e" num={5} conf={false}></Box>
-          <Box letter="f" num={6} conf={false}></Box>
-          <img className="pipi" src={Pipi} alt="Pipi"></img>
           <img className="ladder" src={LeftLadder} alt="LeftLadder"></img>
           <img className="ladder" src={RightLadder} alt="RightLadder"></img>
           <div id="inbox-wrapper">
