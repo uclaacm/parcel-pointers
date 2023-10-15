e39f10a4179fd51b66b0318405ec4e8d92859478
lawtlee <lawsac2947@gmail.com>
2023-10-15 10:09:32 -0700

    Updated Terminal.tsx, Terminal.scss
    Added a little alert at the top of the screen
    to alert you when you copy the code.

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -4,6 +4,7 @@ import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import cpp from 'react-syntax-highlighter/dist/esm/languages/hljs/cpp';
 import atomOneLightCustom from '../styles/Terminal/atom-one-light-custom';
 import '../styles/Terminal/Terminal.scss';
+import { useState } from 'react';
 
 export interface TerminalProps {
   code: string;
@@ -12,18 +13,37 @@ export interface TerminalProps {
 SyntaxHighlighter.registerLanguage('cpp', cpp);
 
 export default function Terminal(props: TerminalProps): JSX.Element {
-  return (
-    <div className="terminal">
-      <CopyToClipboard text={props.code}>
-        <FaRegCopy className="copybutton" />
-      </CopyToClipboard>
+  const [alert, setAlert] = useState(false);
 
-      <SyntaxHighlighter
-        language="cpp"
-        style={atomOneLightCustom} // this error doesn't affect the custom theme, will fix later
-      >
-        {props.code}
-      </SyntaxHighlighter>
+  const alertFunction = () => {
+    setAlert(true);
+    setTimeout(() => {
+      setAlert(false);
+    }, 1500);
+  };
+
+  return (
+    <div>
+      <div className={alert ? 'copyalert fadeout' : 'hiddenalert'}>
+        <p>Copied to Clipboard</p>
+      </div>
+      <div className="terminal">
+        <CopyToClipboard
+          text={props.code}
+          onCopy={() => {
+            alertFunction();
+          }}
+        >
+          <FaRegCopy className="copybutton" />
+        </CopyToClipboard>
+
+        <SyntaxHighlighter
+          language="cpp"
+          style={atomOneLightCustom} // this error doesn't affect the custom theme, will fix later
+        >
+          {props.code}
+        </SyntaxHighlighter>
+      </div>
     </div>
   );
 }
