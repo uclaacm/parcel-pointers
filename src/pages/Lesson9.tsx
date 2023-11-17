89c3107d70ee99a518734f920182398dc8da0236
Yixin Hu <huyixin@oschina.cn>
2023-11-17 15:10:17 -0800

    fix explanations

--- src/pages/Lesson9.tsx
+++ src/pages/Lesson9.tsx
@@ -4,9 +4,9 @@ import lesson9pic2 from '../../public/lesson9pic2.png';
 import lesson9pic3 from '../../public/lesson9pic3.png';
 import AppWrapper from '../components/AppWrapper';
 import NavButtons from '../components/NavButtons';
+import Note from '../components/Note';
 import Terminal from '../components/Terminal';
 import { HeaderSections } from '../types/globalTypes';
-import Note from '../components/Note';
 import '../styles/Lesson9.scss';
 
 const Lesson9: FC = () => {
@@ -25,12 +25,12 @@ const Lesson9: FC = () => {
           <h1>Pointers in Linked Lists</h1>
           <p>
             We are going to take a look at one of the most common applications
-            of pointers: linked lists. .
+            of pointers: linked lists.
           </p>
           <p>
             Linked lists are a string of nodes that each contain a value and a
             pointer to the next node. Generally, they start with a head pointer
-            to the first node, then that node points to the next, and so on
+            to the first node, then that node points to the next, and so on.
           </p>
           <h2>Simple Implementation of a Node Class</h2>
 
@@ -53,7 +53,7 @@ const Lesson9: FC = () => {
           </div>
 
           <p>
-            We can also make this a <strong> circularly linked </strong>list by
+            We can also make this a <strong>circularly linked </strong>list by
             making the final next pointer actually point back to the first node.
           </p>
 
@@ -64,10 +64,10 @@ const Lesson9: FC = () => {
           <p>
             We have been talking about singly linked lists so far, since we only
             have a pointer that points to the next node, but there are also
-            <strong> doubly linked lists </strong>. Doubly linked lists also
-            have a pointer that goes backwards through the list. See this
-            example of tracing a linked list through the tail pointer, a pointer
-            that points to the last node.
+            <strong>doubly linked lists</strong>. Doubly linked lists also have
+            a pointer that goes backwards through the list. See this example of
+            tracing a linked list through the tail pointer, a pointer that
+            points to the last node.
           </p>
 
           <div className="lesson9-diagram">
@@ -77,14 +77,18 @@ const Lesson9: FC = () => {
           <div className="diagram">
             <Note title="Note: What is nullptr">
               <p>
-                <strong>nullptr </strong>in C++ is the null pointer. In C++ it
-                actually has a different meaning from <strong>NULL</strong>{' '}
-                (which is actually equal to 0). It is a keyword that
-                specifically means that a pointer does not point to anything, so
-                generally you should use nullptr when dealing with pointers. If
-                a nullptr is <strong>dereferenced</strong>, it is{' '}
+                <strong>nullptr</strong> in C++ is the{' '}
+                <strong>null pointer</strong>. In C++ it actually has a
+                different meaning from the NULL keyword (which is actually equal
+                to 0). It is a keyword that specifically means that a pointer
+                does not point to anything, so generally it is only used when
+                initializing new pointers. If a nullptr is{' '}
+                <strong>dereferenced</strong>, it is{' '}
                 <strong>undefined behavior</strong> and most of the time your
-                program will hang (never finish execution).
+                program will hang (never finish execution). It is possible to
+                make a pointer <strong>NULL</strong>(basically pointing to
+                address 0), but this is undefined behavior as well if the
+                pointer is dereferenced.
               </p>
             </Note>
           </div>
