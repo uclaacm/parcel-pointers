83c59dc2fc40843c3fc267c50a15f43aa8454c42
Chengheng Li <li471115381@gmail.com>
2024-02-05 21:15:58 -0800

    Refactoring execise 2 code to make it responsive, tried to make the less changes possible

--- src/components/SelectCode.tsx
+++ src/components/SelectCode.tsx
@@ -3,8 +3,8 @@ import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import * as React from 'react';
-
 import { useState } from 'react';
+import '../styles/SelectCode.scss'; // Import the SCSS file for styles
 
 interface SelectCodeProps {
   choices: string[];
@@ -20,24 +20,21 @@ const SelectCode: React.FC<SelectCodeProps> = ({ choices, handleClick }) => {
   };
 
   return (
-    <Box sx={{ width: 'fit-content', height: '5vh', fontFamily: 'monospace' }}>
-      <FormControl fullWidth>
+    <Box className="selectBox" sx={{ fontFamily: 'monospace' }}> {/* Use className for targeting in SCSS */}
+      <FormControl fullWidth className='formControl'>
         <Select
           value={display}
           onChange={handleChange}
           displayEmpty
           inputProps={{ 'aria-label': 'Without label' }}
-          sx={{
-            fontFamily: 'monospace',
-            borderColor: '#CFCFCF',
-            border: '12px',
-          }}
+          className="selectInput" // Added className for targeting the Select component in SCSS
         >
           {choices.map((choice, index) => (
             <MenuItem
               value={choice}
               key={index}
-              sx={{ fontFamily: 'monospace' }}
+              className="menuItem" // Added className for targeting the MenuItem component in SCSS
+              sx={{ whiteSpace: 'normal', wordWrap: 'break-word' }} // Added styles for text wrapping
             >
               {choice}
             </MenuItem>
