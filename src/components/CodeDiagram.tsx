fd888ffd9339756f391fa2a566a56576cdad6694
Hunter Kang <hunterkang5@g.ucla.edu>
2024-02-08 12:46:26 -0800

    fixed images names

--- src/components/CodeDiagram.tsx
+++ src/components/CodeDiagram.tsx
@@ -1,10 +1,10 @@
 import '../styles/CodeDiagram.scss';
-import {useState} from 'react';
+import { useState } from 'react';
 import '../styles/Terminal/Terminal.scss';
 import FilledDot from '../../public/FilledDot.png';
-import lesson4pic1 from '../../public/lesson4pic1.svg';
-import lesson4pic2 from '../../public/lesson4pic2.svg';
-import lesson4pic3 from '../../public/lesson4pic3.svg';
+import lesson5pic1 from '../../public/lesson5pic1.svg';
+import lesson5pic2 from '../../public/lesson5pic2.svg';
+import lesson5pic3 from '../../public/lesson5pic3.svg';
 import RegDot from '../../public/RegDot.png';
 
 export interface CodeDiagramProps {
@@ -39,29 +39,29 @@ export default function CodeDiagram(props: CodeDiagramProps): JSX.Element {
         <div className="arrow">
           {diagram === 0 && (
             <div className="resizedimage">
-              <img src={lesson4pic1} alt="Lesson 4 Pic1"/>
+              <img src={lesson5pic1} alt="Lesson 5 Pic1" />
             </div>
           )}
           {diagram === 1 && (
             <div className="resizedimage1">
-              <img src={lesson4pic2} alt="Lesson 4 Pic2"/>
+              <img src={lesson5pic2} alt="Lesson 5 Pic2" />
             </div>
           )}
           {diagram == 2 && (
             <div className="resizedimage1">
-              <img src={lesson4pic3} alt="Lesson 4 Pic3"/>
+              <img src={lesson5pic3} alt="Lesson 5 Pic3" />
             </div>
           )}
         </div>
       </div>
       <div className="indicators">
-          {props.code.map((_, index) => (
-            <img
-              key={index}
-              src={clicked === index ? FilledDot : RegDot}
-              alt={`Indicator ${index + 1}`}
-            />
-          ))}
+        {props.code.map((_, index) => (
+          <img
+            key={index}
+            src={clicked === index ? FilledDot : RegDot}
+            alt={`Indicator ${index + 1}`}
+          />
+        ))}
       </div>
     </div>
   );
