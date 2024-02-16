17c11e4d0d47b124865ca37ea269246108bea1bf
Daniel Kao <dack8@outlook.com>
2024-02-16 00:15:53 -0800

    fix lint

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -12,20 +12,24 @@ export default function NavButtons(props: NavProps): JSX.Element {
   const pageSelect = PageOrder;
   return (
     <div>
-      {!props.noPrev && <button
-        onClick={() => (window.location.href = pageSelect[props.page - 1])}
-        className="previous"
-      >
-        <ArrowBackIosNewIcon fontSize="large" />
-        <span className="previous-text">Previous</span>
-      </button>}
-      {!props.noNext && <button
-        onClick={() => (window.location.href = pageSelect[props.page + 1])}
-        className="next"
-      >
-        <span className="next-text">Next</span>
-        <ArrowForwardIosIcon fontSize="large" />
-      </button>}
+      {!props.noPrev && (
+        <button
+          onClick={() => (window.location.href = pageSelect[props.page - 1])}
+          className="previous"
+        >
+          <ArrowBackIosNewIcon fontSize="large" />
+          <span className="previous-text">Previous</span>
+        </button>
+      )}
+      {!props.noNext && (
+        <button
+          onClick={() => (window.location.href = pageSelect[props.page + 1])}
+          className="next"
+        >
+          <span className="next-text">Next</span>
+          <ArrowForwardIosIcon fontSize="large" />
+        </button>
+      )}
     </div>
   );
 }
