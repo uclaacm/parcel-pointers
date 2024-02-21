758a885faa91b8f89a84fcabfc12934511d04b28
Yuqi Huang <yuki19@g.ucla.edu>
2024-02-21 11:47:48 -0800

    further add dropdown options

--- src/pages/Exercise6.tsx
+++ src/pages/Exercise6.tsx
@@ -6,9 +6,12 @@ const question = [
   {
     options: [
       'head = new Node();\nhead->next = new Node();\nhead->next = head;\ntail = head->next;',
+      'head = new Node();\nhead->next = new Node();\nhead->next = head;\ntail = head->next;',
+      '*head = new Node();\n*(head->next) = new Node();\n*(head->next->prev) = *head;\ntail = *head->next;',
+      'head = new Node();\nhead-> next = new Node();\ntail = head->next;\nhead->next->prev = head;',
     ],
     answer:
-      'head = new Node();\nhead->next = new Node();\nhead->next = head;\ntail = head->next;',
+      'head = new Node();\nhead-> next = new Node();\ntail = head->next;\nhead->next->prev = head;',
     correctText:
       'Correct! Let’s go through this line by line. The first line creates a new node and assigns head to point to it. The second line creates a second node and assigns the head node’s next pointer to point to it. The third line assigns tail to point at the second node, since it copies the head node’s next pointer. The fourth line actually simplifies to tail->prev = *head, since from the third line we know tail = head->next, it assigns the previous pointer of the tail node to point back to the head node.',
     wrongText: 'Not quite',
@@ -20,7 +23,7 @@ const Exercise6: FC = () => {
     <div className="exercise6-div">
       <RunCode
         questions={question}
-        displayText={'Which code segment correctly starts the linked list?'}
+        displayText={'Which code segment correctly starts the linked list?\n Node* head; // head Node, assume Node class is defined\nNode* tail; // tail Node'}
       />
     </div>
   );
